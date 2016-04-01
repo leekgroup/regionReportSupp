60645f8d89e2d12a1c2fd47f8d38da10ab5a9323
lcolladotor <lcollado@jhsph.edu>
2016-04-01 01:33:41 -0400

    Major update. Re-added bumphunter example as a backup to Bioc. Adapted to to https://github.com/rstudio/rmarkdown/issues/248 being fixed

---
+++ bumphunterExample.R
@@ -0,0 +1,17 @@
+## Script for creating a backup of the bumphunterExample in case that it's not
+## available via Bioconductor.
+
+file.copy(file.path('..', 'regionReport', 'vignettes', 'bumphunterExample.Rmd'), '.', overwrite = TRUE)
+
+original <- readLines('bumphunterExample.Rmd')
+new <- gsub('bumphunterExampleOutput.html', '/regionReportSupp/bumphunter-example/index.html', original)
+new <- gsub('bumphunterExampleOutput', 'index', new)
+new[grep('vignetteInfo.Rmd', new)[c(1, 3)]] <- gsub('vignetteInfo.Rmd', 'bumphunter-example/vignetteInfo.Rmd', new[grep('vignetteInfo.Rmd', new)[c(1, 3)]])
+new <- new[-grep('case the link does not work', new)]
+new <- gsub("outdir = '.'", "outdir = 'bumphunter-example'", new)
+
+writeLines(new[-c(8:11)], 'bumphunterExample.Rmd')
+
+dir.create('bumphunter-example', showWarnings = FALSE)
+rmarkdown::render('bumphunterExample.Rmd')
+
