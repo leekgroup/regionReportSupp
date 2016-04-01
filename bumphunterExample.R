31ea864fe6892aecc484277ef4eaa940df3c7862
lcolladotor <lcollado@jhsph.edu>
2016-04-01 05:02:14 -0400

    Update to run with newest regionReport

--- bumphunterExample.R
+++ bumphunterExample.R
@@ -6,7 +6,9 @@ file.copy(file.path('..', 'regionReport', 'vignettes', 'bumphunterExample.Rmd'),
 original <- readLines('bumphunterExample.Rmd')
 new <- gsub('bumphunterExampleOutput.html', '/regionReportSupp/bumphunter-example/index.html', original)
 new <- gsub('bumphunterExampleOutput', 'index', new)
-new[grep('vignetteInfo.Rmd', new)[c(1, 3)]] <- gsub('vignetteInfo.Rmd', 'bumphunter-example/vignetteInfo.Rmd', new[grep('vignetteInfo.Rmd', new)[c(1, 3)]])
+i <- grep('regionReportBumphunter.Rmd', new)
+new[i] <- gsub('regionReportBumphunter.Rmd', file.path(getwd(),
+    'bumphunter-example', 'regionReportBumphunter.Rmd'), new[i])
 new <- new[-grep('case the link does not work', new)]
 new <- gsub("outdir = '.'", "outdir = 'bumphunter-example'", new)
 
@@ -14,4 +16,4 @@ writeLines(new[-c(8:11)], 'bumphunterExample.Rmd')
 
 dir.create('bumphunter-example', showWarnings = FALSE)
 rmarkdown::render('bumphunterExample.Rmd')
-
+file.remove('bumphunter-example/index.Rmd')
